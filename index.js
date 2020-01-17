b4fbc79e42a2a3ac04a1a3f0fd11435b56c738c7
arellaTV <jeremiah.arella@gmail.com>
2020-01-17 15:25:19 -0800

    Renames base request var to megaphone

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 const MEGAPHONE_API_PODCAST_URL = process.env.MEGAPHONE_API_PODCAST_URL || "";
 const MEGAPHONE_API_TOKEN = process.env.MEGAPHONE_API_TOKEN || "";
 
-const baseRequest = request.defaults({
+const megaphone = request.defaults({
     headers: {
         'Authorization': `Token token=${MEGAPHONE_API_TOKEN}`
     }
 });
 
-baseRequest.get(MEGAPHONE_API_PODCAST_URL, (err, response, body) => {
+megaphone.get(MEGAPHONE_API_PODCAST_URL, (err, response, body) => {
     if (err) {
         console.warn(err);
         return;
