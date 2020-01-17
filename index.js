76adcce881d18713271822980dfa1515efda38b0
arellaTV <jeremiah.arella@gmail.com>
2020-01-17 15:13:38 -0800

    Gets the latest 100 episodes for a given podcast

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-require('dotenv').config()
+const request = require('request');
+require('dotenv').config();
+
+const MEGAPHONE_API_PODCAST_URL = process.env.MEGAPHONE_API_PODCAST_URL || "";
+const MEGAPHONE_API_TOKEN = process.env.MEGAPHONE_API_TOKEN || "";
+
+const baseRequest = request.defaults({
+    headers: {
+        'Authorization': `Token token=${MEGAPHONE_API_TOKEN}`
+    }
+});
+
+baseRequest.get(MEGAPHONE_API_PODCAST_URL, (err, response, body) => {
+    if (err) {
+        console.warn(err);
+        return;
+    }
+
+    const episodes = JSON.parse(body);
+    console.log({ episodeLength: episodes.length });
+});
