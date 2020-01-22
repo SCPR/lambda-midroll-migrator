e1f03cc31b2bb3bffa93c216f814d80d9f498563
arellaTV <jeremiah.arella@gmail.com>
2020-01-22 15:18:33 -0800

    Finds matching episodes

--- index.js
+++ index.js
@@ -3,19 +3,55 @@ require('dotenv').config();
 
 const MEGAPHONE_API_PODCAST_URL = process.env.MEGAPHONE_API_PODCAST_URL || "";
 const MEGAPHONE_API_TOKEN = process.env.MEGAPHONE_API_TOKEN || "";
+const OMNYSTUDIO_CONSUMER_API_PODCAST_URL = process.env.OMNYSTUDIO_CONSUMER_API_PODCAST_URL || "";
+const OMNYSTUDIO_MANAGEMENT_API_TOKEN = process.env.OMNYSTUDIO_MANAGEMENT_API_TOKEN || "";
 
-const megaphone = request.defaults({
+const megaphoneApi = request.defaults({
     headers: {
         'Authorization': `Token token=${MEGAPHONE_API_TOKEN}`
     }
 });
 
-megaphone.get(MEGAPHONE_API_PODCAST_URL, (err, response, body) => {
+const omnyStudioConsumerApi = request;
+const omnyStudioManagementApi = request.defaults({
+    headers: {
+        'Authorization': `OmnyToken ${OMNYSTUDIO_MANAGEMENT_API_TOKEN}`
+    }
+});
+
+const getMegaphoneMidrolls = (title) => {
+    megaphoneApi.get(MEGAPHONE_API_PODCAST_URL, (err, response, body) => {
+        if (err) {
+            console.warn(err);
+            return;
+        }
+
+        const episodes = JSON.parse(body);
+        const episode = episodes.filter(episode => episode.title.includes(title));
+        console.log({ episodeWeGotFromMegaphone: episode });
+    });
+}
+
+// Get an episode list from omny studio and find the first episode that is missing midrolls
+omnyStudioConsumerApi.get(OMNYSTUDIO_CONSUMER_API_PODCAST_URL, (err, response, body) => {
     if (err) {
         console.warn(err);
         return;
     }
 
-    const episodes = JSON.parse(body);
-    console.log({ episodeLength: episodes.length });
+    const parsedBody = JSON.parse(body);
+
+    let index = 0;
+    let clip;
+    while (parsedBody.Clips[index] && !clip) {
+        if (parsedBody.Clips[index].Monetization.MidRolls.length === 0) {
+            clip = parsedBody.Clips[index];
+            console.log('Found the first appearance of a flip without midroll');
+            console.log({ clip: clip.Title });
+            // Find the equivalent episode in megaphone and check if it has a midroll
+            getMegaphoneMidrolls(clip.Title);
+        } else {
+            index++;
+        }
+    }
 });
