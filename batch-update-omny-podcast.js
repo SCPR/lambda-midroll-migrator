bad5f737075f4aee3a02d2d535b3352c099f50f4
arellaTV <jeremiah.arella@gmail.com>
2020-01-28 15:40:29 -0800

    Adds psuedo code for batch update script

---
+++ batch-update-omny-podcast.js
@@ -0,0 +1,11 @@
+// Get all episoes of a program from Omny Studio
+
+// Get all episodes of a program from Megaphone
+
+    // Filter to only the episodes that have midroll
+
+    // For every episode, find the equivalent episode in Omny
+
+    // Perform a put request
+
+// Report number of episodes missing midrolls in Omny Studio that have midrolls in Megaphone
