aafcf955a3ceba558269c7faa189d63d001e0f70
arellaTV <jeremiah.arella@gmail.com>
2020-01-29 15:17:44 -0800

    Matches megaphone and omny episodes together

--- batch-update-omny-podcast.js
+++ batch-update-omny-podcast.js
@@ -1,11 +1,31 @@
 // Get all episoes of a program from Omny Studio
 
+const omnyStudioEpisodes = require('./airtalk-omnystudio-stitched.json');
+console.log(omnyStudioEpisodes.Clips.length);
+
 // Get all episodes of a program from Megaphone
 
-    // Filter to only the episodes that have midroll
+const megaphoneEpisodes = require('./airtalk-megaphone.json');
+console.log(megaphoneEpisodes.length);
 
-    // For every episode, find the equivalent episode in Omny
+// Filter to only the episodes that have midroll
 
-    // Perform a put request
+const megaphoneEpisodesWithMidroll = megaphoneEpisodes.filter(episode => episode.insertionPoints.length > 0);
+console.log(megaphoneEpisodesWithMidroll.length);
 
-// Report number of episodes missing midrolls in Omny Studio that have midrolls in Megaphone
+// For every episode, find the equivalent episode in Omny
+const matchedEpisodes = [];
+const unmatchedEpisodes = [];
+megaphoneEpisodesWithMidroll.forEach(megaphoneEpisode => {
+    const matchingOmnyEpisode = omnyStudioEpisodes.Clips.filter(episode => megaphoneEpisode.title.includes(episode.Title));
+    if (matchingOmnyEpisode.length > 0) {
+        matchedEpisodes.push(megaphoneEpisode);
+    } else {
+        unmatchedEpisodes.push(megaphoneEpisode);
+    }
+});
+
+console.log({ matchedEpisodesLength: matchedEpisodes.length });
+console.log({ unmatchedEpisodesLength: unmatchedEpisodes.length });
+
+// Perform a put request
