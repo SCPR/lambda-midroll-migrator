c413784d240c7bde9c69c04ea10f743ea060a06c
arellaTV <jeremiah.arella@gmail.com>
2020-01-29 15:23:08 -0800

    Adds dependencies

--- batch-update-omny-podcast.js
+++ batch-update-omny-podcast.js
@@ -1,3 +1,23 @@
+const request = require('request');
+require('dotenv').config();
+
+const MEGAPHONE_API_PODCAST_URL = process.env.MEGAPHONE_API_PODCAST_URL || "";
+const MEGAPHONE_API_TOKEN = process.env.MEGAPHONE_API_TOKEN || "";
+const OMNYSTUDIO_MANAGEMENT_API_PODCAST_URL = process.env.OMNYSTUDIO_MANAGEMENT_API_PODCAST_URL || "";
+const OMNYSTUDIO_MANAGEMENT_API_TOKEN = process.env.OMNYSTUDIO_MANAGEMENT_API_TOKEN || "";
+
+const megaphoneApi = request.defaults({
+    headers: {
+        'Authorization': `Token token=${MEGAPHONE_API_TOKEN}`
+    }
+});
+
+const omnyStudioManagementApi = request.defaults({
+    headers: {
+        'Authorization': `OmnyToken ${OMNYSTUDIO_MANAGEMENT_API_TOKEN}`
+    }
+});
+
 // Get all episoes of a program from Omny Studio
 
 const omnyStudioEpisodes = require('./airtalk-omnystudio-stitched.json');
