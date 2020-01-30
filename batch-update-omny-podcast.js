385e74d418341940c9d53db93f429d453166505f
arellaTV <jeremiah.arella@gmail.com>
2020-01-29 16:00:50 -0800

    Applies the midrolls when given 2 jsons

--- batch-update-omny-podcast.js
+++ batch-update-omny-podcast.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const promiseSerial = require('promise-serial');
 require('dotenv').config();
 
 const MEGAPHONE_API_PODCAST_URL = process.env.MEGAPHONE_API_PODCAST_URL || "";
@@ -39,7 +40,7 @@ const unmatchedEpisodes = [];
 megaphoneEpisodesWithMidroll.forEach(megaphoneEpisode => {
     const matchingOmnyEpisode = omnyStudioEpisodes.Clips.filter(episode => megaphoneEpisode.title.includes(episode.Title));
     if (matchingOmnyEpisode.length > 0) {
-        matchedEpisodes.push(megaphoneEpisode);
+        matchedEpisodes.push({ megaphoneEpisode: megaphoneEpisode, omnyEpisode: matchingOmnyEpisode[0] });
     } else {
         unmatchedEpisodes.push(megaphoneEpisode);
     }
@@ -49,3 +50,58 @@ console.log({ matchedEpisodesLength: matchedEpisodes.length });
 console.log({ unmatchedEpisodesLength: unmatchedEpisodes.length });
 
 // Perform a put request
+
+const updateOmnyEpisodeWithMidrolls = (clipId, midrolls, title) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log({ clipId, midrolls, title });
+            omnyStudioManagementApi.post({
+                url: `https://api.omnystudio.com/v0/clips/${clipId}`,
+                body: {
+                    "Monetization": {
+                      "MidRolls": midrolls
+                    }
+                },
+                json: true
+            }, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                    console.warn(err);
+                    return;
+                }
+                console.log(body);
+                resolve(body);
+            });
+        }, 2000);
+    });
+}
+
+const uniqueMidrolls = (midrolls) => {
+    if (midrolls) {
+        return [...new Set(midrolls)];
+    } else {
+        return [];
+    }
+}
+
+// Transform the midrolls from seconds to HH:MM:SS
+const transformMidrolls = (midrollArray) => {
+    const transformedMidrolls = midrollArray.map((insertionPoint) => {
+        return new Date(insertionPoint * 1000).toISOString().substr(11, 8);
+    });
+
+    return uniqueMidrolls(transformedMidrolls);
+}
+
+const testEpisodeList = matchedEpisodes;
+
+const promises = testEpisodeList.map((episode) => {
+    const midrolls = episode.megaphoneEpisode.insertionPoints;
+    const transformedMidrolls = transformMidrolls(midrolls);
+    const clipId = episode.omnyEpisode.Id;
+    const title = episode.omnyEpisode.Title;
+
+    return updateOmnyEpisodeWithMidrolls.bind(this, clipId, transformedMidrolls, title);
+});
+
+promiseSerial(promises);
